f43b4f1a001752f13bce99e646a4f0fbec4cab1b
smit_mewada <smit.mewada@instinctinnovations.com>
2025-05-11 11:46:49 +0530

    Added function to compare user input password and encrypted password

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,6 +1,7 @@
 const UserRepository = require("../repository/user-repository");
 const jwt = require("jsonwebtoken");
 const {JWT_KEY} = require('../config/serverConfig');
+const bcrypt = require('bcrypt');
 
 class UserService{
   constructor(){
@@ -33,6 +34,16 @@ class UserService{
       return result;
     } catch (error) {
         console.log("Something went wrong while token validation", error);
+        throw error;
+    }
+  }
+
+  checkPassword(userInputPlainPassword, encryptedPassword){
+    try {
+      return bcrypt.compareSync(userInputPlainPassword, encryptedPassword);
+      
+    } catch (error) {
+      console.log("Something went wrong while password verification", error);
         throw error;
     }
   }
